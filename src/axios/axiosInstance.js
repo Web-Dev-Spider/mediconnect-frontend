c1171510b6edb4c1ddebd901854b68f70f503e3c
Syam <nmsyam.mohan@gmail.com>
2025-04-14 23:20:57 +0530

    dotenv corrected

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const authInstance = axios.create({
-  baseURL: "http://localhost:2000/api/auth",
+  // baseURL: "http://localhost:2000/api/auth",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/auth`,
 });
 
 export default authInstance;
