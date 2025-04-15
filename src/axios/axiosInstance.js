cef5f1be91c3fef3bf564eb5d03b1a5bb90801b4
Syam <nmsyam.mohan@gmail.com>
2025-04-15 20:05:36 +0530

    base url corrected for axios

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authInstance = axios.create({
   // baseURL: "http://localhost:2000/api/auth",
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/auth`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/auth`,
 });
 
 export default authInstance;
